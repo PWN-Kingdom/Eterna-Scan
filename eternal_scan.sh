7fb84be928453f336e8b94858ad93c1a0140c866
PWN-Kingdom <66555259+PWN-Kingdom@users.noreply.github.com>
2020-08-03 11:09:18 +0000

    Update eternal_scan.sh

--- eternal_scan.sh
+++ eternal_scan.sh
@@ -1,64 +1,31 @@
-# Change IP or PORT. Ranges also work
+# Change IP or PORT. Range is not working now.
 
 ip=""
 port="445 3389"
 
-ALL=`nmap -A --script=smb-vuln* -p $port $ip | tr " " "\n" | egrep -o -e "([0-9]{1,3}[\.]){3}[0-9]{1,3}$" -e "CVE-2017-0143" -e ^"[0-9]{1,5}/"`
-
-# If you want to save the entire scan result.
-# Comment out line 6 and uncomment lines 11 13
-
-#nmap -A --script=smb-vuln* -p $port $ip > result.txt
-
-#ALL=`cat result.txt | tr " " "\n" | egrep -o -e "([0-9]{1,3}[\.]){3}[0-9]{1,3}$" -e "CVE-2017-0143" -e ^"[0-9]{1,5}/"`
-
-if [[ $? -eq 0 ]]
-then
+for IP in $ip
+do
+	echo " ------------------------------------------------------- "
 	CURRENT_CVE=" "
-	for ARG in $ALL
+	for PORT in $port
 	do
-		TEMP=`echo $ARG | egrep "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
-		if [[ $? -eq 0 ]]				# IP
+		CURRENT_CVE=" "
+		ALL=`nmap -A --script=smb-vuln-ms17-010.nse -p $PORT $IP | tr " " "\n" | egrep -o "CVE-2017-0143"`
+		if [[ $? -eq 0 ]]
 		then
-			IP=$TEMP
-			if [[ $IP != $CURRENT_IP ]]
-			then
-				if [[ "$CURRENT_CVE" == "" ]]
+			for ARG in $ALL
+			do
+				CVE=`echo $ARG | egrep "CVE-2017-0143"`
+				echo $CURRENT_CVE | grep -o $CVE &>/dev/null
+				if [[ $? -eq 1 ]]
 				then
-					echo "|	$CURRENT_IP:$PORT	|	Nothing		|"
+					echo "|	$IP:$PORT	|	$CVE	|"	
 				fi
-				echo " ------------------------------------------------------- "
-				CURRENT_IP=$IP
-				CURRENT_CVE=""
-			fi
+				CURRENT_CVE+=$CVE" "
+			done
 		else
-			TEMP=`echo $ARG | egrep ^"[0-9]{1,5}/"`
-			if [[ $? -eq 0 ]]			# PORT
-			then
-				PORT=`echo $TEMP | tr '/' ' '`
-				if [[ $PORT != $CURRENT_PORT ]]
-				then
-					CURRENT_CVE=""
-					CURRENT_PORT=$PORT
-				fi
-			else
-				TEMP=`echo $ARG | egrep "CVE-2017-0143"`
-				if [[ $? -eq 0 ]]		# CVE
-				then
-					CVE=$TEMP
-					echo $CURRENT_CVE | grep -o $CVE &>/dev/null
-					if [[ $? -eq 1 ]]
-					then
-						echo "|	$IP:$PORT	|	$CVE	|"	
-					fi
-					CURRENT_CVE+=$CVE" "
-				fi
-			fi
+			echo "|	$IP:$PORT	|	Nothing		|"
 		fi
 	done
-	if [[ "$CURRENT_CVE" == "" ]]
-	then
-		echo "|	$CURRENT_IP:$PORT	|	Nothing		|"
-	fi
-	echo " ------------------------------------------------------- "
-fi
+done
+echo " ------------------------------------------------------- "
